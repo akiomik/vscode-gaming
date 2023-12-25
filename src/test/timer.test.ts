5a1e12c1703c39b0d52b9707ef411a1173317959
Akiomi Kamakura <akiomik@gmail.com>
2023-12-25 20:21:18 +0900

    Add tests for Timer

---
+++ src/test/timer.test.ts
@@ -0,0 +1,45 @@
+import * as assert from 'assert';
+
+import FakeTimers, { type InstalledClock } from '@sinonjs/fake-timers';
+import { Timer } from '../timer';
+
+suite('Timer', () => {
+  let clock: InstalledClock;
+  let a = 0;
+
+  suiteSetup(() => {
+    clock = FakeTimers.install();
+  });
+
+  suiteTeardown(() => {
+    clock.uninstall();
+  });
+
+  test('.getInstance', () => {
+    assert.strictEqual(Timer.getInstance(), Timer.getInstance());
+  });
+
+  test('#start', () => {
+    const timer = Timer.getInstance();
+    timer.start(() => {
+      a += 42;
+    }, 1000);
+    clock.tick(1000);
+    assert.strictEqual(a, 42);
+  });
+
+  test('#stop', () => {
+    const timer = Timer.getInstance();
+    timer.stop();
+    clock.tick(1000);
+    assert.strictEqual(a, 42);
+  });
+
+  test('#isRunning', () => {
+    const timer = Timer.getInstance();
+    timer.start(() => {}, 1000);
+    assert.equal(timer.isRunning(), true);
+    timer.stop();
+    assert.equal(timer.isRunning(), false);
+  });
+});
