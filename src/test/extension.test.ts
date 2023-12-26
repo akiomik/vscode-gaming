8af7ff7e9abb8be250b7fb24fcd5fb34a590be86
Akiomi Kamakura <akiomik@gmail.com>
2023-12-26 20:57:15 +0900

    Improve extension tests

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,7 +1,8 @@
 import * as assert from 'assert';
+import { setTimeout } from 'node:timers/promises';
+import FakeTimers, { type InstalledClock } from '@sinonjs/fake-timers';
 import * as vscode from 'vscode';
 
-import FakeTimers, { type InstalledClock } from '@sinonjs/fake-timers';
 import { Timer } from '../timer';
 
 suite('Commands', () => {
@@ -23,27 +24,39 @@ suite('Commands', () => {
   });
 
   test('vscode-gaming.start', async () => {
-    const config = vscode.workspace.getConfiguration();
+    let config = vscode.workspace.getConfiguration();
     const timer = Timer.getInstance();
     assert.deepEqual(config.get('workbench.colorCustomizations'), {});
     assert.equal(timer.isRunning(), false);
 
     await vscode.commands.executeCommand('vscode-gaming.start');
-    clock.tick(100);
+    clock.tick(50);
+    await setTimeout(1000);
 
+    config = vscode.workspace.getConfiguration();
     assert.equal(timer.isRunning(), true);
-    // assert.deepEqual(config.get('workbench.colorCustomizations'), {});
+    assert.notDeepEqual(config.get('workbench.colorCustomizations'), {});
   });
 
   test('vscode-gaming.stop', async () => {
-    // const config = vscode.workspace.getConfiguration();
     const timer = Timer.getInstance();
-    // assert.deepEqual(config.get('workbench.colorCustomizations'), {});
     assert.equal(timer.isRunning(), true);
 
     await vscode.commands.executeCommand('vscode-gaming.stop');
 
     assert.equal(timer.isRunning(), false);
-    // assert.deepEqual(config.get('workbench.colorCustomizations'), {});
+  });
+
+  test('vscode-gaming.reset', async () => {
+    let config = vscode.workspace.getConfiguration();
+    const timer = Timer.getInstance();
+    assert.notDeepEqual(config.get('workbench.colorCustomizations'), {});
+
+    await vscode.commands.executeCommand('vscode-gaming.reset');
+    await setTimeout(1000);
+
+    config = vscode.workspace.getConfiguration();
+    assert.equal(timer.isRunning(), false);
+    assert.deepEqual(config.get('workbench.colorCustomizations'), {});
   });
 });
